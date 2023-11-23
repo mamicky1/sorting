d750873e3cb2b56d7e31bb033d15a46a1f4d744e
mamicky1 <abdulmaleeq2017@gmail.com>
2023-11-23 07:38:45 -0800

    addedsorting

--- s.js
+++ s.js
@@ -1,11 +1,23 @@
-for i from 1 to n-1:
-    key = arr[i]
-    j = i - 1
-    
-    // Move elements of arr[0..i-1] that are greater than key
-    // to one position ahead of their current position
-    while j >= 0 and arr[j] > key:
-        arr[j + 1] = arr[j]
-        j = j - 1
-    
-    arr[j + 1] = key
+function insertionSort(arr) {
+    let n = arr.length;
+
+    for (let i = 1; i < n; i++) {
+        let key = arr[i];
+        let j = i - 1;
+
+        // Move elements of arr[0..i-1] that are greater than key
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+// Example usage:
+let arrayToSort = [5, 2, 4, 6, 1, 3];
+insertionSort(arrayToSort);
+console.log("Sorted Array:", arrayToSort);
+
